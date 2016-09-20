80a4db3df8d1ebd8f1e0eb5410bd447975bf4346
Digital Advancements <digitaladvancements@gmail.com>
2016-09-19 19:53:55 -0600

    Current theme
    Current theme on live server

--- front-page.php
+++ front-page.php
@@ -1,55 +1,55 @@
-<?php get_header(); ?> 
- <header>
-    <h1>CodeKracken</h1>
-    <p>Kracken the code in web design</p>
-</header>
-<section class="tiles">
- <?php 
-     $args = array(
-    'meta_key' => 'order',
-    'orderby' => 'meta_value',
-    'order' => 'ASC'
-);
-    query_posts( $args, 'post_type=post&cat=13'); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-/* * See if the current post is in category html5. 
-   * If it is, the div is given the CSS class "post-category-three".
-   * Otherwise, the div is given the CSS class "post".
-*/
-if ( in_category( 'html5' ) ) : ?>
-<article class="style1">
-    <?php elseif ( in_category( 'css3' )) : ?>
-<article class="style2">
-   <?php elseif ( in_category( 'javascript' )) : ?>
-   <article class="style3">
-   <?php elseif ( in_category( 'php' )) : ?>
-   <article class="style4">
-   <?php elseif ( in_category( 'wordpress' )) : ?>
-   <article class="style5">
-   <?php elseif ( in_category( 'reseller-hosting' )) : ?>
-   <article class="style6">
-    <?php endif; ?>
- <span class="image">
-    <?php the_post_thumbnail(); ?>
-</span>
-        <a href="<?php the_permalink(); ?>">
-        <h2><?php the_title() ?></h2>
-        <div class="content">
-            <?php the_excerpt(); ?>
-        </div>
-       </a>
-    </article>
-<?php endwhile; else :
-/*
- * The very first "if" tested to see if there were any posts to
- * display. This "else" part tells what do if there weren't any.
- */
-_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
- // Completely stop the Loop.
- endif;
-       
-       wp_reset_query();
-?>
-   
-    </section>			
+<?php get_header(); ?> 
+ <header>
+    <h1>CodeKracken</h1>
+    <p>Kracken the code in web design</p>
+</header>
+<section class="tiles">
+ <?php 
+     $args = array(
+    'meta_key' => 'order',
+    'orderby' => 'meta_value',
+    'order' => 'ASC'
+);
+    query_posts( $args, 'post_type=post&cat=13'); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+/* * See if the current post is in category html5. 
+   * If it is, the div is given the CSS class "post-category-three".
+   * Otherwise, the div is given the CSS class "post".
+*/
+if ( in_category( 'html5' ) ) : ?>
+<article class="style1">
+    <?php elseif ( in_category( 'css3' )) : ?>
+<article class="style2">
+   <?php elseif ( in_category( 'javascript' )) : ?>
+   <article class="style3">
+   <?php elseif ( in_category( 'php' )) : ?>
+   <article class="style4">
+   <?php elseif ( in_category( 'wordpress' )) : ?>
+   <article class="style5">
+   <?php elseif ( in_category( 'reseller-hosting' )) : ?>
+   <article class="style6">
+    <?php endif; ?>
+ <span class="image">
+    <?php the_post_thumbnail(); ?>
+</span>
+        <a href="<?php the_permalink(); ?>">
+        <h2><?php the_title() ?></h2>
+        <div class="content">
+            <?php the_excerpt(); ?>
+        </div>
+       </a>
+    </article>
+<?php endwhile; else :
+/*
+ * The very first "if" tested to see if there were any posts to
+ * display. This "else" part tells what do if there weren't any.
+ */
+_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+ // Completely stop the Loop.
+ endif;
+       
+       wp_reset_query();
+?>
+   
+    </section>			
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,69 +1,52 @@
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
-		<!--<link rel="stylesheet" href="wp-content/themes/codekracken/assets/css/main.css">-->
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-		<!-- Wrapper -->
-			<div id="wrapper">
-
-				<!-- Header -->
-					<header id="header">
-						<div class="inner">
-
-							<!-- Logo -->
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
-									<span class="symbol">
-									<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="">
-									<!--<img src="images/logo.svg" alt="" />-->
-									</span>
-									<span class="title"><?php bloginfo( 'name' ); ?></span>
-								</a>
-
-							<!-- Nav -->
-								<nav>
-									<ul>
-										<li><a href="#menu">Menu</a></li>
-									</ul>
-								</nav>
-
-						</div>
-					</header>
-
-				<!-- Menu -->
-					<nav id="menu">
-						<h2>Menu</h2>
-						<?php 
-                            wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => false ) );
-                        ?>
-					</nav>
-<!-- Main -->
-					<div id="main">
-						<div class="inner">
-						
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--<link rel="stylesheet" href="wp-content/themes/codekracken/assets/css/main.css">-->
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
+									<span class="symbol">
+									<img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="">
+									<!--<img src="images/logo.svg" alt="" />-->
+									</span>
+									<span class="title"><?php bloginfo( 'name' ); ?></span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<?php 
+                            wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => false ) );
+                        ?>
+					</nav>
+<!-- Main -->
+					<div id="main">
+						<div class="inner">
+						
 					
